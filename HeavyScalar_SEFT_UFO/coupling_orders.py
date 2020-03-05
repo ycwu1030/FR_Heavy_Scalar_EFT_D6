20bae460338c633d1e43b1889f486b6841a88745
Yongcheng Wu <yc_wu1030@126.com>
2020-03-05 18:44:44 -0500

    Isolate out SM part

---
+++ HeavyScalar_SEFT_UFO/coupling_orders.py
@@ -0,0 +1,24 @@
+# This file was automatically created by FeynRules 2.3.32
+# Mathematica version: 10.4.1 for Mac OS X x86 (64-bit) (April 17, 2016)
+# Date: Thu 5 Mar 2020 18:42:52
+
+
+from object_library import all_orders, CouplingOrder
+
+
+EFF4 = CouplingOrder(name = 'EFF4',
+                     expansion_order = 99,
+                     hierarchy = 2)
+
+EFF6 = CouplingOrder(name = 'EFF6',
+                     expansion_order = 99,
+                     hierarchy = 2)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
